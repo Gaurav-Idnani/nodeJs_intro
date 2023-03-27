bd606a4b811b70745aa4b639b7c736425de00fa0
Gaurav Idnani <gaurav.idnani@v2force.com>
2023-03-27 22:19:41 +0530

    Converting to async await

--- index.js
+++ index.js
@@ -5,10 +5,15 @@ const fs = require ('fs');
 const path = require ('path');
 const { promisify } = require ('util');
 const readFile = promisify(fs.readFile);
-const filename = path.resolve('\data','Contacts_Data.csv');
-readFile(filename,'utf8')
-    .then((file)=>{
+const filename = path.resolve('\data','Contact_Data.csv');
+
+async function readFileAsyncAwait() {
+    try{
+        const file =await readFile(filename,'utf8');
         console.log (file);
-    }).catch(err=>{
+    }catch (err){
         console.error (err);
-    })
+    }
+}
+
+readFileAsyncAwait();
